a11a7070b31690b6462c0b3a41ce3cc7a3f3a556
Kevin Falcone <kevin@edx.org>
2015-08-06 14:05:14 -0400

    Add a very basic setup.py so I can pip install this from requirements

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+setup(name='awspricingfull',
+      version='2.0',
+      py_modules=['awspricingfull'],
+      url='https://github.com/ilia-semenov/awspricingfull',
+      author='Ilia Semenov'
+)
